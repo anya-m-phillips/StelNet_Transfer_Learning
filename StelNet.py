b44720b07ec4dd5568f072cc589b02c4cc81f725
anya-m-phillips <anyamphillilps@gmail.com>
2023-09-04 14:40:21 -0400

    Add transfer learning routines to StelNet.py, and a demo of the TL routine to StelNet_example.ipynb (required adding MIST files to Aux/)

--- StelNet.py
+++ StelNet.py
@@ -102,6 +102,14 @@ def predict(X, n=20, TL=None):
         # Transfer Learning from SAO REU 2023
         pre_model = "Models/TL/reu2023_preZAMS_TL/ONC_NGC6530_xtra_lowhighmassMIST{}"
         post_model = "Models/TL/reu2023_postZAMS_TL/FGK_DH_GR_xtra_lowhighmassMIST{}" 
+
+
+    ## add more TL options here for models made using transfer_learning_bootstraps()
+    ### example:
+    if TL=="example":
+        # example for github
+        # not altering pre_model for now
+        post_model = "Models/TL/new_TL/github_example/postms_tl_example{}"
     
     net_preMS.load_state_dict(torch.load(pre_model.format(0)), strict=False)
     net_postMS.load_state_dict(torch.load(post_model.format(0)), strict=False)
@@ -232,3 +240,169 @@ def plot_gaussian_posteriors(y_gaussian_posteriors, obs_id, log_age_range=[4,12]
     ax[1].text(log_mass_range[1]-(log_mass_range[1]-log_mass_range[0])/3,20, 'obs_id={}'.format(obs_id), fontsize=30)
     
     return ax
+
+def transfer_learning_bootstraps(data, n_trs, n_trs_mist, modelname, stage, n_it=100, start_from_baseline=True, starting_point=None):
+    """
+    Function to perform transfer learning for any data
+    
+    Arguments:
+        data: list - list of arrays that look like ((logTeff, logLum, logAge, logMass), ...) -  
+        n_trs: list - list of integers that are ~about how many stars from each dataset in datas you want in the training sample
+        n_trs_mist: int - rough number of stars you want in MIST training set (should generally match n_trs numbers)
+        modelname: str - path from this directory to where you want to store the trained model; models will save as 'modelname{}'.format(bootstrap number)
+        stage: str - "pre" or "post" (ZAMS); need to add error handeling
+        n_it: int - number of iterations to train for (defaults to 100)
+        start_from_baseline: bool - whether to start from a baseline model (defaults to True)
+        starting_point: str - if start_from_baseline==False, starting_point is the path to the model that will be re-trained instead
+
+    Returns:
+        no returns; saves 20 numbered models to the path specified with modelname
+    """
+
+    # normalize data
+    norm_x_data = []
+    norm_y_data = []
+
+    norm_min_preMS = np.load('Aux/norm_min_preMS.npy')
+    norm_max_preMS = np.load('Aux/norm_max_preMS.npy')
+    
+    norm_min_postMS = np.load('Aux/norm_min_postMS.npy')
+    norm_max_postMS = np.load('Aux/norm_max_postMS.npy')
+    
+    for i in range(len(data)):
+        dataset=data[i]
+        x_data_preMS, x_data_postMS = normalize(dataset)
+
+        y_data_preMS = torch.from_numpy((dataset[:,2:4]-norm_min_preMS[2:])/(norm_max_preMS[2:]-norm_min_preMS[2:])).float()
+        y_data_postMS = torch.from_numpy((dataset[:,2:4]-norm_min_postMS[2:])/(norm_max_postMS[2:]-norm_min_postMS[2:])).float()
+        
+        if stage=="pre":
+            norm_x_data.append(x_data_preMS)
+            norm_y_data.append(y_data_preMS)
+        if stage=="post":
+            norm_x_data.append(x_data_postMS)
+            norm_y_data.append(y_data_postMS)
+
+            
+    # split into train/test
+    tr_list = []
+    ts_list = []
+    for i in range(len(data)):
+        x_data = norm_x_data[i]
+        y_data = norm_y_data[i]
+        n_tr = n_trs[i]
+
+        
+        tr_sz = n_tr/len(x_data)
+        idx_tr = np.random.rand(x_data.shape[0])<tr_sz
+
+        x_dset_tr = x_data[idx_tr,:]
+        y_dset_tr = y_data[idx_tr,:]
+
+        x_dset_ts = x_data[~idx_tr,:]
+        y_dset_ts = y_data[~idx_tr,:]
+
+        data_tr = torch.concat((x_dset_tr, y_dset_tr), 1)
+        tr_list.append(data_tr)
+
+        data_ts = torch.concat((x_dset_ts, y_dset_ts),1)
+        ts_list.append(data_ts)
+    
+    # add MIST data to training set
+    if stage=="pre":
+        mist_tl = torch.from_numpy(np.load('Aux/mist_tl_preMS.npy', allow_pickle=True)).float()
+    if stage=="post":
+        mist_tl = torch.from_numpy(np.load('Aux/mist_tl_postMS.npy',allow_pickle=True)).float()
+
+    tr_sz_mist = n_trs_mist/len(mist_tl)
+    idx_tr_mist = np.random.rand(mist_tl.shape[0])<tr_sz_mist
+    mist_tl_tr = mist_tl[idx_tr_mist,:]
+    mist_tl_ts = mist_tl[~idx_tr_mist,:]
+
+    tr_list.append(mist_tl_tr[:,:4])
+    ts_list.append(mist_tl_ts[:,:4])                                 
+    
+    
+    # concatenate training data
+    all_tr = torch.cat(tr_list, 0)   
+    
+
+    # specify params
+    D_in = 2
+    D_out = 2
+    num_layers = 10
+    num_nodes =50
+    activation = nn.ReLU() 
+    
+    # define separate networks for pre/postMS, pre/postMS transfer learning
+    net = NN(D_in, D_out, num_layers, num_nodes, activation)
+    net_preMS = NN(D_in, D_out, num_layers, num_nodes, activation)
+    net_postMS = NN(D_in, D_out, num_layers, num_nodes, activation)
+    net_preMS_tl = NN(D_in, D_out, num_layers, num_nodes, activation)
+    net_postMS_tl = NN(D_in, D_out, num_layers, num_nodes, activation)
+
+    # define full model:
+    num_layers = 10
+    num_nodes =50
+    activation = nn.ReLU()    
+    net = NN(D_in, D_out, num_layers, num_nodes, activation)
+    
+    # Perform optimization
+    loss_fn = nn.MSELoss(reduction='sum')  
+    optim = torch.optim.Adam(net.parameters(),lr=1e-3) 
+    
+    num_iterations = n_it
+    
+    sz=all_tr.shape[0]
+
+    n_samples=sz
+    
+    ### THE TRANSFER LEARNING HAPPENS HERE! ###
+    for i in range(20):                
+        #net.apply(weight_reset)  # don't need since we're starting with a loaded model every time ?
+
+
+        if start_from_baseline==True:
+            if stage=="pre":
+                net.load_state_dict(torch.load('Models/Baseline/mist_baseline_preMS10'), strict=False) 
+            
+            if stage=="post":
+                net.load_state_dict(torch.load('Models/Baseline/mist_baseline_postMS10'), strict=False) 
+
+        if start_from_baseline==False:
+            net.load_state_dict(torch.load(starting_point), strict=False)
+        
+        x_data = all_tr[:,:2] 
+        y_data = all_tr[:,2:] 
+
+        # the sampling with replacement for this bootstrap
+        data_idx = np.random.choice(sz, n_samples)
+        x_data = x_data[data_idx]
+        y_data = y_data[data_idx]
+
+        
+        Loss = []
+        
+        for j in range(num_iterations):
+            y_data = y_data.reshape(-1,2)   
+    
+            # run the model forward on the data
+            y_pred = net(x_data).squeeze(-1)   # ?
+            
+            # calculate the mse loss
+            loss = loss_fn(y_pred, y_data)
+            
+            # initialize gradients to zero
+            optim.zero_grad()
+            
+            # backpropagate
+            loss.backward() 
+            
+            # take a gradient step
+            optim.step()            
+            
+            Loss.append(loss)
+            if (j + 1) % 500 == 0:
+                print("[iteration %04d] loss: %.4f" % (j + 1, loss.item()))
+    
+        torch.save(net.state_dict(), modelname+'{}'.format(i))   
